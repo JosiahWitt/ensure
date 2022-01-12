d3cc6e25b5c5f708149995ba2c4bbda4822b32fd
Josiah Witt <josiah@jwitt.me>
2022-01-11 20:31:06 -0500

    Add cmd package

---
+++ cmd/ensure/internal/cmd/mocks.go
@@ -0,0 +1,102 @@
+package cmd
+
+import (
+	"github.com/JosiahWitt/ensure/cmd/ensure/internal/ensurefile"
+	"github.com/JosiahWitt/ensure/cmd/ensure/internal/ifacereader"
+	"github.com/JosiahWitt/ensure/cmd/ensure/internal/uniqpkg"
+	"github.com/urfave/cli/v2"
+)
+
+func (a *App) mocksCmd() *cli.Command {
+	return &cli.Command{
+		Name:  "mocks",
+		Usage: "commands related to mocks",
+
+		Subcommands: []*cli.Command{
+			a.mocksGenerateCmd(),
+			a.mocksTidyCmd(),
+		},
+	}
+}
+
+func (a *App) mocksGenerateCmd() *cli.Command {
+	return &cli.Command{
+		Name:  "generate",
+		Usage: "generates GoMocks (https://github.com/golang/mock) for the packages and interfaces listed in .ensure.yml",
+
+		Action: func(c *cli.Context) error {
+			pwd, err := a.Getwd()
+			if err != nil {
+				return err
+			}
+
+			config, err := a.EnsureFileLoader.LoadConfig(pwd)
+			if err != nil {
+				return err
+			}
+
+			pkgList := buildPackageList(config.Mocks.Packages)
+			packageImports := uniqpkg.New()
+
+			a.Logger.Println("Reading packages listed in .ensure.yml...")
+
+			pkgs, err := a.InterfaceReader.ReadPackages(pkgList, packageImports)
+			if err != nil {
+				return err
+			}
+
+			a.Logger.Println("Generating mocks...")
+
+			mocks, err := a.MockGenerator.GenerateMocks(pkgs, packageImports)
+			if err != nil {
+				return err
+			}
+
+			if err := a.MockWriter.WriteMocks(config, mocks); err != nil {
+				return err
+			}
+
+			if config.Mocks.TidyAfterGenerate {
+				if err := a.MockWriter.TidyMocks(config); err != nil {
+					return err
+				}
+			}
+
+			return nil
+		},
+	}
+}
+
+func (a *App) mocksTidyCmd() *cli.Command {
+	return &cli.Command{
+		Name:  "tidy",
+		Usage: "removes any files and directories that would not be generated for the packages and interfaces listed in .ensure.yml",
+
+		Action: func(c *cli.Context) error {
+			pwd, err := a.Getwd()
+			if err != nil {
+				return err
+			}
+
+			config, err := a.EnsureFileLoader.LoadConfig(pwd)
+			if err != nil {
+				return err
+			}
+
+			return a.MockWriter.TidyMocks(config)
+		},
+	}
+}
+
+func buildPackageList(configPackages []*ensurefile.MockPackage) []*ifacereader.PackageDetails {
+	packages := make([]*ifacereader.PackageDetails, 0, len(configPackages))
+
+	for _, pkg := range configPackages {
+		packages = append(packages, &ifacereader.PackageDetails{
+			Path:       pkg.Path,
+			Interfaces: pkg.Interfaces,
+		})
+	}
+
+	return packages
+}
---
+++ cmd/ensure/internal/cmd/mocks_test.go
@@ -0,0 +1,369 @@
+package cmd_test
+
+import (
+	"errors"
+	"io/ioutil"
+	"log"
+	"testing"
+
+	"github.com/JosiahWitt/ensure"
+	"github.com/JosiahWitt/ensure/cmd/ensure/internal/cmd"
+	"github.com/JosiahWitt/ensure/cmd/ensure/internal/ensurefile"
+	"github.com/JosiahWitt/ensure/cmd/ensure/internal/ifacereader"
+	"github.com/JosiahWitt/ensure/cmd/ensure/internal/mockgen"
+	"github.com/JosiahWitt/ensure/cmd/ensure/internal/mocks/mock_ensurefile"
+	"github.com/JosiahWitt/ensure/cmd/ensure/internal/mocks/mock_ifacereader"
+	"github.com/JosiahWitt/ensure/cmd/ensure/internal/mocks/mock_mockgen"
+	"github.com/JosiahWitt/ensure/cmd/ensure/internal/mocks/mock_mockwrite"
+	"github.com/JosiahWitt/ensure/cmd/ensure/internal/uniqpkg"
+	"github.com/JosiahWitt/ensure/ensurepkg"
+)
+
+func TestMocksGenerate(t *testing.T) {
+	ensure := ensure.New(t)
+
+	type Mocks struct {
+		EnsureFileLoader *mock_ensurefile.MockLoaderIface
+		IfaceReader      *mock_ifacereader.MockReadable
+		MockGen          *mock_mockgen.MockGenerator
+		MockWriter       *mock_mockwrite.MockWritable
+	}
+
+	exampleError := errors.New("something went wrong")
+	defaultWd := func() (string, error) {
+		return "/test", nil
+	}
+
+	table := []struct {
+		Name          string
+		ExpectedError error
+		Flags         []string
+
+		Getwd      func() (string, error)
+		Mocks      *Mocks
+		SetupMocks func(*Mocks)
+		Subject    *cmd.App
+	}{
+		{
+			Name:  "with valid execution",
+			Getwd: defaultWd,
+			SetupMocks: func(m *Mocks) {
+				m.EnsureFileLoader.EXPECT().LoadConfig("/test").Return(buildConfig(configNoop), nil)
+
+				pkgsImports := uniqpkg.New()
+
+				m.IfaceReader.EXPECT().
+					ReadPackages(buildIfaceReaderPackagesInput(), pkgsImports).
+					Return(buildIfaceReaderPackagesOutput(), nil)
+
+				m.MockGen.EXPECT().
+					GenerateMocks(buildIfaceReaderPackagesOutput(), pkgsImports).
+					Return(buildGeneratedMocks(), nil)
+
+				m.MockWriter.EXPECT().WriteMocks(buildConfig(configNoop), buildGeneratedMocks())
+			},
+		},
+		{
+			Name:  "with valid execution and tidy mocks is enabled",
+			Getwd: defaultWd,
+			SetupMocks: func(m *Mocks) {
+				m.EnsureFileLoader.EXPECT().LoadConfig("/test").Return(buildConfig(configTidyEnabled), nil)
+
+				pkgsImports := uniqpkg.New()
+
+				m.IfaceReader.EXPECT().
+					ReadPackages(buildIfaceReaderPackagesInput(), pkgsImports).
+					Return(buildIfaceReaderPackagesOutput(), nil)
+
+				m.MockGen.EXPECT().
+					GenerateMocks(buildIfaceReaderPackagesOutput(), pkgsImports).
+					Return(buildGeneratedMocks(), nil)
+
+				m.MockWriter.EXPECT().WriteMocks(buildConfig(configTidyEnabled), buildGeneratedMocks())
+
+				m.MockWriter.EXPECT().TidyMocks(buildConfig(configTidyEnabled))
+			},
+		},
+		{
+			Name:  "returns error when unable to get workind directory",
+			Getwd: func() (string, error) { return "", exampleError },
+
+			ExpectedError: exampleError,
+		},
+		{
+			Name:  "returns error when unable to load config",
+			Getwd: defaultWd,
+
+			ExpectedError: exampleError,
+
+			SetupMocks: func(m *Mocks) {
+				m.EnsureFileLoader.EXPECT().LoadConfig("/test").Return(nil, exampleError)
+			},
+		},
+		{
+			Name:  "returns error when unable to read packages",
+			Getwd: defaultWd,
+
+			ExpectedError: exampleError,
+
+			SetupMocks: func(m *Mocks) {
+				m.EnsureFileLoader.EXPECT().LoadConfig("/test").Return(buildConfig(configTidyEnabled), nil)
+
+				pkgsImports := uniqpkg.New()
+
+				m.IfaceReader.EXPECT().
+					ReadPackages(buildIfaceReaderPackagesInput(), pkgsImports).
+					Return(nil, exampleError)
+			},
+		},
+		{
+			Name:  "returns error when unable to generate mocks",
+			Getwd: defaultWd,
+
+			ExpectedError: exampleError,
+
+			SetupMocks: func(m *Mocks) {
+				m.EnsureFileLoader.EXPECT().LoadConfig("/test").Return(buildConfig(configTidyEnabled), nil)
+
+				pkgsImports := uniqpkg.New()
+
+				m.IfaceReader.EXPECT().
+					ReadPackages(buildIfaceReaderPackagesInput(), pkgsImports).
+					Return(buildIfaceReaderPackagesOutput(), nil)
+
+				m.MockGen.EXPECT().
+					GenerateMocks(buildIfaceReaderPackagesOutput(), pkgsImports).
+					Return(nil, exampleError)
+			},
+		},
+		{
+			Name:  "returns error when unable to write mocks",
+			Getwd: defaultWd,
+
+			ExpectedError: exampleError,
+
+			SetupMocks: func(m *Mocks) {
+				m.EnsureFileLoader.EXPECT().LoadConfig("/test").Return(buildConfig(configNoop), nil)
+
+				pkgsImports := uniqpkg.New()
+
+				m.IfaceReader.EXPECT().
+					ReadPackages(buildIfaceReaderPackagesInput(), pkgsImports).
+					Return(buildIfaceReaderPackagesOutput(), nil)
+
+				m.MockGen.EXPECT().
+					GenerateMocks(buildIfaceReaderPackagesOutput(), pkgsImports).
+					Return(buildGeneratedMocks(), nil)
+
+				m.MockWriter.EXPECT().WriteMocks(buildConfig(configNoop), buildGeneratedMocks()).Return(exampleError)
+			},
+		},
+		{
+			Name:  "returns error when unable to tidy mocks",
+			Getwd: defaultWd,
+
+			ExpectedError: exampleError,
+
+			SetupMocks: func(m *Mocks) {
+				m.EnsureFileLoader.EXPECT().LoadConfig("/test").Return(buildConfig(configTidyEnabled), nil)
+
+				pkgsImports := uniqpkg.New()
+
+				m.IfaceReader.EXPECT().
+					ReadPackages(buildIfaceReaderPackagesInput(), pkgsImports).
+					Return(buildIfaceReaderPackagesOutput(), nil)
+
+				m.MockGen.EXPECT().
+					GenerateMocks(buildIfaceReaderPackagesOutput(), pkgsImports).
+					Return(buildGeneratedMocks(), nil)
+
+				m.MockWriter.EXPECT().WriteMocks(buildConfig(configTidyEnabled), buildGeneratedMocks())
+
+				m.MockWriter.EXPECT().TidyMocks(buildConfig(configTidyEnabled)).Return(exampleError)
+			},
+		},
+	}
+
+	ensure.RunTableByIndex(table, func(ensure ensurepkg.Ensure, i int) {
+		entry := table[i]
+		entry.Subject.Getwd = entry.Getwd
+		entry.Subject.Logger = log.New(ioutil.Discard, "", 0)
+
+		err := entry.Subject.Run(append([]string{"ensure", "mocks", "generate"}, entry.Flags...))
+		ensure(err).IsError(entry.ExpectedError)
+	})
+}
+
+func TestMocksTidy(t *testing.T) {
+	ensure := ensure.New(t)
+
+	type Mocks struct {
+		EnsureFileLoader *mock_ensurefile.MockLoaderIface
+		MockWriter       *mock_mockwrite.MockWritable
+	}
+
+	exampleError := errors.New("something went wrong")
+	defaultWd := func() (string, error) {
+		return "/test", nil
+	}
+
+	table := []struct {
+		Name          string
+		ExpectedError error
+
+		Getwd      func() (string, error)
+		Mocks      *Mocks
+		SetupMocks func(*Mocks)
+		Subject    *cmd.App
+	}{
+		{
+			Name:  "with valid execution",
+			Getwd: defaultWd,
+			SetupMocks: func(m *Mocks) {
+				m.EnsureFileLoader.EXPECT().
+					LoadConfig("/test").
+					Return(&ensurefile.Config{
+						RootPath: "/some/root/path",
+					}, nil)
+
+				m.MockWriter.EXPECT().
+					TidyMocks(&ensurefile.Config{
+						RootPath: "/some/root/path",
+					}).
+					Return(nil)
+			},
+		},
+		{
+			Name:          "returns error when unable to load working directory",
+			Getwd:         func() (string, error) { return "", exampleError },
+			ExpectedError: exampleError,
+		},
+		{
+			Name:          "returns error when unable to load config",
+			Getwd:         defaultWd,
+			ExpectedError: exampleError,
+			SetupMocks: func(m *Mocks) {
+				m.EnsureFileLoader.EXPECT().LoadConfig("/test").Return(nil, exampleError)
+			},
+		},
+		{
+			Name:          "returns error when unable to tidy mocks",
+			Getwd:         defaultWd,
+			ExpectedError: exampleError,
+			SetupMocks: func(m *Mocks) {
+				m.EnsureFileLoader.EXPECT().
+					LoadConfig("/test").
+					Return(&ensurefile.Config{
+						RootPath: "/some/root/path",
+					}, nil)
+
+				m.MockWriter.EXPECT().
+					TidyMocks(&ensurefile.Config{
+						RootPath: "/some/root/path",
+					}).
+					Return(exampleError)
+			},
+		},
+	}
+
+	ensure.RunTableByIndex(table, func(ensure ensurepkg.Ensure, i int) {
+		entry := table[i]
+		entry.Subject.Getwd = entry.Getwd
+
+		err := entry.Subject.Run([]string{"ensure", "mocks", "tidy"})
+		ensure(err).IsError(entry.ExpectedError)
+	})
+}
+
+func configNoop(config *ensurefile.Config) {}
+func configTidyEnabled(config *ensurefile.Config) {
+	config.Mocks.TidyAfterGenerate = true
+}
+
+func buildConfig(modify func(config *ensurefile.Config)) *ensurefile.Config {
+	config := &ensurefile.Config{
+		RootPath: "/some/root/path",
+		Mocks: &ensurefile.MockConfig{
+			Packages: []*ensurefile.MockPackage{
+				{
+					Path:       "pkgs/pkg1",
+					Interfaces: []string{"Iface1", "Iface2"},
+				},
+				{
+					Path:       "pkgs/pkg2",
+					Interfaces: []string{"Iface3", "Iface4"},
+				},
+			},
+		},
+	}
+
+	modify(config)
+	return config
+}
+
+func buildIfaceReaderPackagesInput() []*ifacereader.PackageDetails {
+	return []*ifacereader.PackageDetails{
+		{
+			Path:       "pkgs/pkg1",
+			Interfaces: []string{"Iface1", "Iface2"},
+		},
+		{
+			Path:       "pkgs/pkg2",
+			Interfaces: []string{"Iface3", "Iface4"},
+		},
+	}
+}
+
+func buildIfaceReaderPackagesOutput() []*ifacereader.Package {
+	return []*ifacereader.Package{
+		{
+			Name: "p1",
+			Path: "pkgs/pkg1",
+			Interfaces: []*ifacereader.Interface{
+				{
+					Name: "Iface1",
+					Methods: []*ifacereader.Method{
+						{Name: "Method"},
+					},
+				},
+				{
+					Name: "Iface2",
+					Methods: []*ifacereader.Method{
+						{Name: "Method"},
+					},
+				},
+			},
+		},
+		{
+			Name: "p2",
+			Path: "pkgs/pkg2",
+			Interfaces: []*ifacereader.Interface{
+				{
+					Name: "Iface3",
+					Methods: []*ifacereader.Method{
+						{Name: "Method"},
+					},
+				},
+				{
+					Name: "Iface4",
+					Methods: []*ifacereader.Method{
+						{Name: "Method"},
+					},
+				},
+			},
+		},
+	}
+}
+
+func buildGeneratedMocks() []*mockgen.PackageMock {
+	return []*mockgen.PackageMock{
+		{
+			Package:      buildIfaceReaderPackagesOutput()[0],
+			FileContents: "pkg1 file!",
+		},
+		{
+			Package:      buildIfaceReaderPackagesOutput()[1],
+			FileContents: "pkg2 file!",
+		},
+	}
+}
