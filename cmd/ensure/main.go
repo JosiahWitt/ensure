ad371b4cbdcf992fb8b5142fb6c73d52148427b6
Josiah Witt <josiah@jwitt.me>
2022-05-19 14:51:49 -0400

    Upgrade CLI dependencies to fix issue loading packages

--- cmd/ensure/main.go
+++ cmd/ensure/main.go
@@ -16,7 +16,7 @@ import (
 //nolint:gochecknoglobals // Allows injecting the version
 // Version of the CLI.
 // Should be tied to the release version.
-var Version = "0.3.0"
+var Version = "0.3.1"
 
 func main() {
 	if err := run(); err != nil {
