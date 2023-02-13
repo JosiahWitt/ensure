f043d5beeea9f1c76f30123bfe9ecd306d842662
Josiah Witt <josiah@jwitt.me>
2023-02-12 20:57:13 -0500

    Add support for grouping a table under a name

--- experiments/entable/entable.go
+++ experiments/entable/entable.go
@@ -1,5 +1,7 @@
 // Package entable provides helpers for constructing table driven tests.
 // It requires Go 1.18+ due to the use of generics.
+//
+// Use [T] to construct a table. Use [G] to nest a table in a group.
 package entable
 
 import "github.com/JosiahWitt/ensure/ensuring"
@@ -40,7 +42,8 @@ import "github.com/JosiahWitt/ensure/ensuring"
 //	})
 type T[E any] []*E
 
-// Run runs the table, executing fn for each entry in the table.
+// Run runs the table, executing fn for each entry in the table. See docs for [T]
+// for an example.
 //
 // It uses [ensuring.E.RunTableByIndex], so the same functionality is supported,
 // including built-in support for mocks.
@@ -52,7 +55,8 @@ func (t T[E]) Run(ensure ensuring.E, fn func(ensure ensuring.E, entry *E)) {
 	})
 }
 
-// RunWithIndex runs the table, executing fn for each entry in the table.
+// RunWithIndex runs the table, executing fn for each entry in the table. See
+// docs for [T] for an example.
 //
 // It uses [ensuring.E.RunTableByIndex], so the same functionality is supported,
 // including built-in support for mocks.
@@ -62,5 +66,79 @@ func (t T[E]) RunWithIndex(ensure ensuring.E, fn func(ensure ensuring.E, i int, 
 	ensure.RunTableByIndex(t, func(ensure ensuring.E, i int) {
 		entry := t[i]
 		fn(ensure, i, entry)
+	})
+}
+
+// G allows constructing a table within a group. It executes all entries
+// in a separate test scope with the provided name.
+//
+// For example:
+//
+//	type Entry struct {
+//		Name    string
+//		Input   string
+//		IsEmpty bool
+//	}
+//
+//	group := entable.G[Entry]{
+//		Name: "grp",
+//		Table: entable.T[Entry]{
+//			{
+//				Name:    "with non empty input",
+//				Input:   "my string",
+//				IsEmpty: false,
+//			},
+//			{
+//				Name:    "with empty input",
+//				Input:   "",
+//				IsEmpty: true,
+//			},
+//		},
+//	}
+//
+//	group.Run(ensure, func(ensure Ensure, entry *Entry) {
+//		// ensure.T().Name() ends with `/grp/<entry name here>`
+//		isEmpty := strs.IsEmpty(entry.Input)
+//		ensure(isEmpty).Equals(entry.IsEmpty)
+//	})
+//
+//	// Or
+//
+//	group.RunWithIndex(ensure, func(ensure Ensure, i int, entry *Entry) {
+//		// ensure.T().Name() ends with `/grp/<entry name here>`
+//		isEmpty := strs.IsEmpty(entry.Input)
+//		ensure(isEmpty).Equals(entry.IsEmpty)
+//	})
+type G[E any] struct {
+	Name  string
+	Table T[E]
+}
+
+// Run runs the table, executing fn for each entry in the table. See docs for [G]
+// for an example.
+//
+// It uses [ensuring.E.RunTableByIndex], so the same functionality is supported,
+// including built-in support for mocks.
+func (g *G[E]) Run(ensure ensuring.E, fn func(ensure ensuring.E, entry *E)) {
+	ensure.T().Helper()
+
+	g.RunWithIndex(ensure, func(ensure ensuring.E, _ int, entry *E) {
+		fn(ensure, entry)
+	})
+}
+
+// RunWithIndex runs the table, executing fn for each entry in the table. See
+// docs for [G] for an example.
+//
+// It uses [ensuring.E.RunTableByIndex], so the same functionality is supported,
+// including built-in support for mocks.
+func (g *G[E]) RunWithIndex(ensure ensuring.E, fn func(ensure ensuring.E, i int, entry *E)) {
+	ensure.T().Helper()
+
+	ensure.Run(g.Name, func(ensure ensuring.E) {
+		ensure.RunTableByIndex(g.Table, func(ensure ensuring.E, i int) {
+			entry := g.Table[i]
+			fn(ensure, i, entry)
+		})
 	})
 }
--- experiments/entable/entable_test.go
+++ experiments/entable/entable_test.go
@@ -125,3 +125,127 @@ func TestTRunWithIndex(t *testing.T) {
 		})
 	})
 }
+
+func TestGRun(t *testing.T) {
+	ensure := ensure.New(t)
+
+	type Entry struct {
+		Name  string
+		Value string
+	}
+
+	ensure.Run("when group has no entries", func(ensure ensuring.E) {
+		group := entable.G[Entry]{}
+
+		calls := 0
+		group.Run(ensure, func(ensure ensuring.E, entry *Entry) {
+			calls++
+		})
+
+		ensure(calls).Equals(0)
+	})
+
+	ensure.Run("when group has entries", func(ensure ensuring.E) {
+		group := entable.G[Entry]{
+			Name: "grp",
+			Table: entable.T[Entry]{
+				{
+					Name:  "first",
+					Value: "hello",
+				},
+				{
+					Name:  "second",
+					Value: "world",
+				},
+			},
+		}
+
+		names := []string{}
+		calls := []*Entry{}
+
+		group.Run(ensure, func(ensure ensuring.E, entry *Entry) {
+			names = append(names, ensure.T().Name())
+			calls = append(calls, entry)
+		})
+
+		ensure(names).Equals([]string{
+			ensure.T().Name() + "/grp/first",
+			ensure.T().Name() + "/grp/second",
+		})
+
+		ensure(calls).Equals([]*Entry{
+			{
+				Name:  "first",
+				Value: "hello",
+			},
+			{
+				Name:  "second",
+				Value: "world",
+			},
+		})
+	})
+}
+
+func TestGRunWithIndex(t *testing.T) {
+	ensure := ensure.New(t)
+
+	type Entry struct {
+		Name  string
+		Value string
+	}
+
+	ensure.Run("when group has no entries", func(ensure ensuring.E) {
+		group := entable.G[Entry]{}
+
+		calls := 0
+		group.RunWithIndex(ensure, func(ensure ensuring.E, i int, entry *Entry) {
+			calls++
+		})
+
+		ensure(calls).Equals(0)
+	})
+
+	ensure.Run("when group has entries", func(ensure ensuring.E) {
+		group := entable.G[Entry]{
+			Name: "grp",
+			Table: entable.T[Entry]{
+				{
+					Name:  "first",
+					Value: "hello",
+				},
+				{
+					Name:  "second",
+					Value: "world",
+				},
+			},
+		}
+
+		indices := []int{}
+		names := []string{}
+		calls := []*Entry{}
+
+		group.RunWithIndex(ensure, func(ensure ensuring.E, i int, entry *Entry) {
+			indices = append(indices, i)
+			names = append(names, ensure.T().Name())
+			calls = append(calls, entry)
+		})
+
+		ensure(indices).Equals([]int{0, 1})
+
+		ensure(names).Equals([]string{
+			ensure.T().Name() + "/grp/first",
+			ensure.T().Name() + "/grp/second",
+		})
+
+		ensure(calls).Equals([]*Entry{
+			{
+				Name:  "first",
+				Value: "hello",
+			},
+			{
+				Name:  "second",
+				Value: "world",
+			},
+		})
+	})
+}
